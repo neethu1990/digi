f96390142fdbe637fa7b3bfaefe56bf5ffdf85ab
Neethu Sunder <neethusunder18@gmail.com>
2025-04-04 02:01:53 +0530

    another commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {  Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import About from "./components/About";
 import Footer from "./components/Footer";
@@ -7,17 +7,17 @@ import Project from "./components/Project";
 
 function App() {
   return (
-    <div style ={{backgroundColor:'blue', backgroundRepeat:'inherit', backgroundSize:'cover' }}>
+    <div>
       
-      
+      <Router>
       < Header />
       <Routes>
-      < Route path="/" element={<Home />} />
+      < Route path="" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/project" element={<Project />} />
       </Routes>
       <Footer />
-    
+      </Router>
     </div>
   );
 }
